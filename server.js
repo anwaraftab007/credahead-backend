28f27be931fbdb2126fc875113553da8c9f351f8
anwaraftab007 <anwaraftab007@gmail.com>
2025-07-21 19:14:31 +0530

    Ping route added

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/contact', contactRoutes);
 app.use('/api/send-demo-request', requestDemoRoutes)
+app.use('/api/ping', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Pong!' });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
